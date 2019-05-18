233b17ad584570c15fc05cf597d4773ff63e0e99
Jonatan Wästlund <jonte.w1@gmail.com>
2019-05-18 12:09:30 +0200

    New code
    REEE

--- Scripts/KinematicBody2D.gd
+++ Scripts/KinematicBody2D.gd
@@ -1,13 +1,20 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.connect("joy_connection_changed",self,"joy_con_changed")
+	
+func _physics_process(delta):
+	move_and_slide(get_input())
 
+func get_input():
+	var motion = Vector2(0,0)
+	if Input.get_connected_joypads().size() > 0:
+		var xAxis = Input.get_joy_axis(0,JOY_AXIS_7)
+		var yAxis = Input.get_joy_axis(0,JOY_AXIS_6)
+		motion.x = xAxis
+		motion.y = yAxis
+	return motion
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
